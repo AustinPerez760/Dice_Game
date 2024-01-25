b52c8d2af3496cbb6025f95717ad4384659aadc5
Austin Perez <austin.perez760@gmail.com>
2024-01-24 19:24:13 -0600

    Completed function to reset the game which is intialized on click of new game button

--- script.js
+++ script.js
@@ -26,10 +26,27 @@ const switchPlayer = () => {
   player1El.classList.toggle('player--active');
 };
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+//Function to reset the game once winner is determined
+const newGame = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.add('player--active');
+};
+newGame();
 
 //Riolling dice funtionality
 btnRoll.addEventListener('click', function () {
@@ -78,4 +95,8 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', function () {
+  newGame();
 });
